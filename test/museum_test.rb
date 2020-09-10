128aec15ae3c67cd3072637d12deea952200eec1
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-10 09:15:51 -0600

    Adds and passes existence with attributes

---
+++ test/museum_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/patron'
+require './lib/exhibit'
+require './lib/museum'
+
+class MuseumTest < Minitest::Test
+
+  def test_it_exitsts_with_attributes
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_instance_of Museum, dmns
+    assert_equal "Denver Museum of Nature and Science", dmns.name
+  end
+
+end

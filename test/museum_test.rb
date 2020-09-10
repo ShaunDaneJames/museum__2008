05a84d9db997d8b9c29aea97d057183df51fc13c
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-10 09:19:36 -0600

    Adds and passes can add exhibits

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -13,4 +13,17 @@ class MuseumTest < Minitest::Test
     assert_equal "Denver Museum of Nature and Science", dmns.name
   end
 
+  def test_starts_with_no_exhibits_then_adds_them
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    assert_equal [], dmns.exhibits
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
+  end
 end

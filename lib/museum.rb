2cef40597834d7afee8b711be117dc4c9ce0d770
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-10 09:34:20 -0600

    Adds and passes recommend exhibits

--- lib/museum.rb
+++ lib/museum.rb
@@ -10,5 +10,17 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
-  
+
+  def patron_intrests_one_at_a_time
+  end
+
+  def recommend_exhibits(patron)
+    recommendations = []
+    @exhibits.find_all do |exhibit|
+      if patron.interests.include?(exhibit.name)
+        recommendations << exhibit
+      end
+    end
+    recommendations
+  end
 end

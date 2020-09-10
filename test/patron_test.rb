db3f45b2ee050ff9aea6655a654d148be148eaa1
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-10 09:13:21 -0600

    Adds interests

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -12,4 +12,13 @@ class PatronTest < Minitest::Test
     assert_equal 20, patron_1.spending_money
   end
 
+  def test_starts_with_no_interests_then_adds_them
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal [], patron_1.interests
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], patron_1.interests
+  end
 end
